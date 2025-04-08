6bab52c4e050351ff3706f7c910208433e29863b
affanbaig111 <affanbaig111@gmail.com>
2025-04-08 12:20:47 +0530

    addded printing statement

--- order-service/src/main/java/com/ms/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/ms/order_service/controller/OrderController.java
@@ -29,8 +29,8 @@ public class OrderController {
     public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException e) {
 
         System.out.println("hey");
-        
 
+        System.out.println("hey");
 
         return CompletableFuture.supplyAsync(() -> "Oops, something went wrong");
 
